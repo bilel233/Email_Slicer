569959c429f57b0509753f440300e7d27f7af548
bilel233 <bilel.mjdb@gmail.com>
2023-11-15 14:08:29 +0100

    creation de la fonction mail et ajout des tests

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ print("=========================================")
 # Generer un programme qui recupere le nom d'utilisateur et le domaine d'un email
 
 print("=========================================")
+
+def mail(mailContent):
+    """renvoie le nom d'utilisateur et le domaine de la messagerie"""
+
+    name,domaine = mailContent.split("@")
+    return name,domaine
+# jeux de tests
+
+print(mail("yoh@mail.com"))
+print(mail("personne@mail.com"))
+
