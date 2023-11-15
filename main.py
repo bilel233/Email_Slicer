89197a76f464f47c2b872b4a2deac35ac0613d64
bilel233 <bilel.mjdb@gmail.com>
2023-11-15 13:47:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("=========================================")
+
+############## Projet Email Slicer ##############
+
+# Generer un programme qui recupere le nom d'utilisateur et le domaine d'un email
+
+print("=========================================")
